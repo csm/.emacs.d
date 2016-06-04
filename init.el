dbbdb1d0efd860ad36b075e6b5c6db2de20f6011
Casey Marshall <casey.s.marshall@seagate.com>
2016-06-04 14:56:20 -0700

    Various changes.

--- init.el
+++ init.el
@@ -66,7 +66,11 @@
     tagedit
 
     ;; git integration
-    magit))
+    magit
+
+    ;; golang
+    go-mode
+    go-complete))
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
@@ -93,7 +97,7 @@
 ;; (require 'yaml-mode)
 ;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 ;; 
-;; Adding this code will make Emacs enter yaml mode whenever you open
+
 ;; a .yml file
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
@@ -130,3 +134,75 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+
+(require 'go-complete)
+(add-hook 'completion-at-point-functions 'go-complete-at-point)
+(setq-default indent-tabs-mode nil)
+
+(defun unfuck-go-mode ()
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq tab-width 4 indent-tabs-mode nil))
+(add-hook 'go-mode-hook 'unfuck-go-mode)
+
+(defun ensimify ()
+  "Run ensimify gradle task on the current project."
+  (interactive)
+  (projectile-with-default-dir (projectile-project-root)
+    (switch-to-buffer-other-window "*ensimify*")
+    (erase-buffer)
+    (start-process "ensimify" "*ensimify*" "bash" "./gradlew" "ensimify")))
+
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
+(defun my-projectile-build ()
+  "Run ./gradlew build in the current project."
+  (interactive)
+  (projectile-with-default-dir (projectile-project-root)
+    (switch-to-buffer-other-window "*gradle-build*")
+    (erase-buffer)
+    (start-process "build" "*gradle-build*" "bash" "./gradlew" "build")))
+
+(defun gradle ()
+  "Run gradle commands in the current project"
+  (interactive)
+  (let ((commands (read-from-minibuffer "Gradle commands:")))
+    (projectile-with-default-dir (projectile-project-root)
+      (switch-to-buffer-other-window "*gradle-command-output*")
+      (erase-buffer)
+      (apply 'start-process (append (list "gradle" "*gradle-command-output*" "bash" "./gradlew") (split-string commands))))))
+
+(defun add-custom-projectile-keys ()
+  (define-key projectile-mode-map [?\s-n] 'projectile-find-tag)
+  (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
+  (define-key projectile-mode-map [?\s-b] 'my-projectile-build))
+(add-hook 'projectile-mode-hook 'add-custom-projectile-keys)
+
+(load-theme 'tango-dark)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#202020" :foreground "#eaeaea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Consolas"))))
+ '(linum ((t (:foreground "#444499"))))
+ '(region ((t (:background "dark blue"))))
+ '(tool-bar ((t (:background "grey75" :foreground "black" :box (:line-width 1 :style released-button) :family "Gill Sans"))))
+ '(tooltip ((t (:inherit variable-pitch :background "lightyellow" :foreground "black" :family "Gill Sans")))))
+
